e63908b8f2934b2b78fc9e42ddfb8ca9a7de2810
htrix <htrixe@gmail.com>
2020-07-15 12:19:05 +0100

    first news api

---
+++ src/news.js
@@ -0,0 +1,8 @@
+// Change YOUR_API_KEY_HERE to your apiKey
+const url =
+  'https://newsapi.org/v2/top-headlines?country=pt&apiKey=YOUR_API_KEY_HERE';
+
+export async function getNews() {
+  let result = await fetch(url).then((response) => response.json());
+  return result.articles;
+}
